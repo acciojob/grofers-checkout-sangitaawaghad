7ef7720962a811bbb61269922974b652998a37bb
sangitaawaghad <44050082+sangitaawaghad@users.noreply.github.com>
2026-05-06 13:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+
+  const prices = document.querySelectorAll('.price');
+
+  let sum = 0;
+
+  prices.forEach(p => {
+    sum += Number(p.textContent);
+  });
+
+
+  const row = document.createElement('tr');
+
+
+  const column = document.createElement('td');
+
+  column.colSpan = 2; // span across table
+  column.textContent = "Total: " + sum;
+
+  row.appendChild(column);
+
+  const table = document.querySelector('table');
+  table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
