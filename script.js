01542dc29ef8e2e7042a683b7475dd55d311554f
sangitaawaghad <44050082+sangitaawaghad@users.noreply.github.com>
2026-05-06 13:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,30 +4,29 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 
-
   const prices = document.querySelectorAll('.prices');
-
   let sum = 0;
 
   prices.forEach(p => {
     sum += Number(p.textContent);
   });
 
+  // Remove old answer if exists
+  const oldAns = document.querySelector('#ans');
+  if (oldAns) oldAns.remove();
 
+  // Create new row
   const row = document.createElement('tr');
 
+  const column = document.createElement('td');
+  column.colSpan = 2;
 
-  const column1 = document.createElement('td');
+  column.id = "ans";   // ⭐ IMPORTANT (for Cypress test)
+  column.textContent = sum;
 
-  // column.colSpan = 2; // span across table
-  column1.textContent = "Total: " ;
-  // const column2 = document.createElement('td');
-  // column2.textContent = sum;
-  row.appendChild(column1);
-	// row.appendChild(column2);
+  row.appendChild(column);
 
-  const table = document.querySelector('table');
-  table.appendChild(row);
+  document.querySelector('table').appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
